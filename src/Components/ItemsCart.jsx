32cc415a89e7ccba9373d9d91198300b363b90de
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-08 19:18:53 +0530

    remove cart  update

--- src/Components/ItemsCart.jsx
+++ src/Components/ItemsCart.jsx
@@ -1,26 +1,40 @@
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 import { useDispatch } from "react-redux";
-import { removeFromCart } from "../Redux/slices/CartSlice";
+import {
+  removeFromCart,
+  decrementQty,
+  incrementQty,
+} from "../Redux/slices/CartSlice";
 
 const ItemsCart = ({ id, name, qty, price, img }) => {
   const dispatch = useDispatch();
-  
+
   return (
     <div className="flex gap-2 shadow-md rounded-lg p-2 mb-3 relative">
       <MdDelete
         className="absolute right-7 text-gray-600 cursor-pointer"
-        onClick={() => dispatch(removeFromCart({id}))} 
+        onClick={() => dispatch(removeFromCart({ id }))}
       />
-      <img src={img} alt={name} className="w-[50px] h-[50px]" />  
+      <img src={img} alt={name} className="w-[50px] h-[50px]" />
       <div className="leading-5">
         <h2 className="font-bold text-gray-800">{name}</h2>
         <div className="flex justify-between">
           <span className="text-green-500 font-bold">₹{price}</span>
           <div className="flex justify-center items-center gap-2 right-7 absolute">
-            <AiOutlinePlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer ease-linear" />
+            <AiOutlineMinus
+              onClick={() =>
+                qty > 1 ? dispatch(decrementQty({ id })) : (qty = 0)
+              }
+              className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer ease-linear"
+            />
             <span>{qty}</span>
-            <AiOutlineMinus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer ease-linear" />
+            <AiOutlinePlus
+              onClick={() =>
+                qty >= 1 ? dispatch(incrementQty({ id })) : (qty = 0)
+              }
+              className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer ease-linear"
+            />
           </div>
         </div>
       </div>
@@ -28,4 +42,4 @@ const ItemsCart = ({ id, name, qty, price, img }) => {
   );
 };
 
-export default ItemsCart;
+export default ItemsCart;
